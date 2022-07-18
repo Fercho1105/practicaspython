e608577a85d5fceb47dade20975037d2a7eabe8f
Fercho1105 <109244480+Fercho1105@users.noreply.github.com>
2022-07-18 14:30:17 -0500

    Semana 4

---
+++ modulo-semana4.py
@@ -0,0 +1,32 @@
+# numHuevos= 12
+
+# #Opcion1
+# print("Tengo " + str(numHuevos) + " huevos.")
+
+# #Opcion2
+# print("Tengo %s huevos." %(numHuevos))
+
+
+###########################################
+
+#Calcular la superficie o área de un cuadrado
+
+# lado=int(input("Ingrese la medida del lado del cuadrado: "))
+# superficie=lado*lado
+# print( "La superficie del cuadrado es: " + str(superficie))
+
+#############################################
+
+nombre=input("Introduce tu nombre: ")
+apellido=input("Introducce tu apellido: ")
+edad=int(input("Introduce tu edad: "))
+correo=input("Introduce tu correo electrónico: ")
+telefono=input("Introduce tu telefono: ")
+
+print("Nombre: " + nombre)
+print("Apellidos: " + apellido)
+print("Edad: " + str(edad))
+print("Correo Electrónico: " + correo)
+print("Telefono: " + telefono)
+
+
