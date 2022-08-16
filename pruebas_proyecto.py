ea251f168e14581f25d4f97cd91a1bbe4ece0f46
Fernando <ferdiazhidalgo1105@gmail.com>
2022-08-15 20:14:37 -0500

    Actualizacion de archivos git

---
+++ pruebas_proyecto.py
@@ -0,0 +1,16 @@
+# nombre=input("Ingrese su nombre: ")
+# numeDeCaracteres_Nombre=len(nombre)
+# tipo_nombre=nombre.isalpha()
+
+# while numeDeCaracteres_Nombre ==0 or tipo_nombre == False:
+#     print("Error, no deje ninguna casilla vacia o ponga algun valor que no sea letras")
+#     nombre=input("Ingrese su nombre: ")
+#     numeDeCaracteres_Nombre=len(nombre)
+#     tipo_nombre=nombre.isalpha()
+#     if numeDeCaracteres_Nombre > 0 and tipo_nombre == True:
+#         break
+
+
+
+
+
